a018642765234ca614f16417eeacef8a4f54a145
UrbanKocmut <u.kocmut@gmail.com>
2018-06-13 17:53:55 +0200

    Added motion sensing

--- app/src/main/java/si/urban/mens/MainActivity.java
+++ app/src/main/java/si/urban/mens/MainActivity.java
@@ -1,13 +1,66 @@
 package si.urban.mens;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor gyroscope;
+    TextView accelTextView;
+    TextView gyroTextView;
+    TextView infoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Hello!");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        accelTextView = (TextView) findViewById(R.id.accelTextView);
+        gyroTextView = (TextView) findViewById(R.id.gyroTextView);
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        infoTextView.setText( String.valueOf(event.sensor.getType()));
+        switch ( event.sensor.getType()) {
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                accelTextView.setText(String.format(Locale.getDefault(), "X:%.2f,Y:%.2f,Z:%.2f", event.values[0], event.values[1], event.values[2]));
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                gyroTextView.setText(String.format(Locale.getDefault(), "X:%.2f,Y:%.2f,Z:%.2f", event.values[0], event.values[1], event.values[2]));
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
