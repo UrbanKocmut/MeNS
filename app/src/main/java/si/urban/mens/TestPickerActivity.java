d90b76efdd60c463552da69098ad95ec15a69bc0
UrbanKocmut <u.kocmut@gmail.com>
2018-08-21 12:53:31 +0200

    Added pin input activity and test selection activity

---
+++ app/src/main/java/si/urban/mens/TestPickerActivity.java
@@ -0,0 +1,13 @@
+package si.urban.mens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestPickerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_picker);
+    }
+}
