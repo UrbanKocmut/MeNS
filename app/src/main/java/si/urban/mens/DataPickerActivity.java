748706492ec4af63304e4179045ae69fb24c5583
Urban <u.kocmut@gmail.com>
2018-08-25 23:59:47 +0200

    Added button to test picker activity and created a new data picker activity

---
+++ app/src/main/java/si/urban/mens/DataPickerActivity.java
@@ -0,0 +1,13 @@
+package si.urban.mens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DataPickerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_picker);
+    }
+}
--- app/src/main/java/si/urban/mens/TestPickerActivity.java
+++ app/src/main/java/si/urban/mens/TestPickerActivity.java
@@ -51,6 +51,7 @@ public class TestPickerActivity extends Activity {
 
         sp.edit().putLong("measurementId",measurementId).apply();
 
+
         Intent intent = new Intent(this, TestActivity.class);
         startActivity(intent);
     }
